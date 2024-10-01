41df90e88da7750724c60575874c4a33d7919cae
Artemburan <artburia17@gmail.com>
2024-10-01 17:55:56 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+distance = float(input("Введіть фути: "))
+inches = distance * 12
+miles = distance * 0.000189393939
+yards = distance * 0.333333333
+print(f"Фути в дюймах {inches}")
+print(f"Фути в милях {miles}")
+print(f"Фути в ярдах {yards}")
